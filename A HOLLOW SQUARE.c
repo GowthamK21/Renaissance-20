947ceab1f9831430d95b7ca6851403768c162057
Gowtham K <46840241+GowthamK21@users.noreply.github.com>
2020-11-09 19:56:00 +0530

    Create A HOLLOW SQUARE.c
    Event Name : Run Time Warrior
    Contest Link : https://www.hackerrank.com/run-time-warrior-renaissance20
    
    Challenge Name : 1.A HOLLOW SQUARE

---
+++ A HOLLOW SQUARE.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=2*n;i++){
+        for(int j=1;j<=2*n;j++){
+            if(i<=n&&j<=n&&i+j<=n+1)
+                printf("%d ",i<j?i:j);
+            else if(i<=n&&j>n&&(2*n-j+1)+i<=n+1)
+                printf("%d ",(2*n-j+1)<i?(2*n-j+1):i);
+            else if(i>n&&j<=n&&(2*n-i+1)+j<=n+1)
+                printf("%d ",(2*n-i+1)<j?(2*n-i+1):j);
+            else if(i>n&&j>n&&(2*n-i+1)+(2*n-j+1)<=n+1)
+                printf("%d ",(2*n-i+1)<(2*n-j+1)?(2*n-i+1):(2*n-j+1));
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}

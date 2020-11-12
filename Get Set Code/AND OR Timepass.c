0af0317e5bac5e5c164798401a3a3adee2a18d95
Gowtham K <46840241+GowthamK21@users.noreply.github.com>
2020-11-12 11:12:02 +0530

    Create AND OR Timepass.c
    Event name : Get Set Code
    Contest link : www.hackerrank.com/get-set-code-renaissance
    Challenge name : AND OR Timepass

---
+++ Get Set Code/AND OR Timepass.c
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+     int n,i,max,res=0;
+    cin>>n;
+    int a[n];
+    cin>>a[0];
+    max=a[0];
+    res=res|a[0];
+    for(i=1;i<n;i++)
+    {
+        cin>>a[i];
+        if(a[i]>max)
+            max=a[i];
+        res=res|a[i];
+    }
+    cout<<max<<" "<<res;
+    return 0;
+}

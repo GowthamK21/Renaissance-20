6049ee1398d5360ed9ab3a6ea8f1a5cec85b499e
Gowtham K <46840241+GowthamK21@users.noreply.github.com>
2020-11-12 11:10:22 +0530

    Create Redundant recursion.c
    Event name : Get Set Code
    Contest link : www.hackerrank.com/get-set-code-renaissance
    Challenge name : Redundant Recursion

---
+++ Get Set Code/Redundant recursion.c
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    long int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"1";
+    }
+    else{
+        cout<<"-1";
+    }
+    return 0;
+}
